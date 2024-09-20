f2d96684f6c5041430345fdf581ded3f8b010906
pirgalim <109841774+pirgalim@users.noreply.github.com>
2024-09-20 03:07:50 -0500

    backup

---
+++ templates.py
@@ -0,0 +1,9 @@
+SAMPLE_SIZE = 50
+
+
+
+### CAMERA TEMPLATES ###
+
+asi6200mm = { 'sensor_x': SAMPLE_SIZE, 'sensor_y': SAMPLE_SIZE, "px_size": 3.86, 
+             'q_efficiency': 0.475, 'read_noise': 1.5, 'gain': 0.779, 
+             'offset': 500, 'dark_noise': 0.0010, 'full_well': 51000 }
