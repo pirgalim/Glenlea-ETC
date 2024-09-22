c0a4055cff700c314814463866137974e322b7c6
pirgalim <109841774+pirgalim@users.noreply.github.com>
2024-09-22 04:06:26 -0500

    cleaning up

--- main.py
+++ main.py
@@ -17,10 +17,6 @@ app.config['SECRET_KEY'] = 'd42c51f24733b869a5916a8c09043624'
 
 
 
-# OMIT_KEY = "   01.  "
-
-
-
 @app.route('/', methods=['GET', 'POST'])
 def calculator():
        
@@ -31,26 +27,12 @@ def calculator():
     # Used for input validation message
     valid = True
       
-    #load presets from templates
-    # camera_presets = readPresets("camera")
-    
-    
-    
-    # print(tp.asi6200mm)
-    
+    # fetch presets
     camera_presets = tp.cameras
     telescope_presets = tp.telescopes
-    
-    # telescope_presets = readPresets("telescope")
-    # filter_presets = readPresets("filter")
-    # target_presets = readPresets("target")
-    
-    # target_presets = pickles()
-        
+            
     # retrieve current GAO SQM value
     gao_sqm = sqm.get_sqm()   
-    
-    
     
     # detect if form has been submitted
     if in_form.submit.data:
@@ -60,25 +42,17 @@ def calculator():
                     
             # retrieve form data
             data = request.form
-            
-            
             params = process_input(data)
-            
-            print("----------\nmain section:\n-------------")
+
+            #TODO for testing
             print(params)
-            
-            
-            # create parameter tuple to be sent to the calculator script
-            # params = readInput(data)
+    
                         
             # validate number of parameters
             if(params is None):
                 return render_template("error.html", message="There is a problem with the number of parameters being passed from the form to the script.")
             
             else:
-                # create instances of the calculator script classes
-                # etc = ETC.Calculator(params)
-                # error = etc.validate()
                 
                 obs = observation.Observation(params)
                 
@@ -87,47 +61,19 @@ def calculator():
                 
                 # validate some parameters                
                 if error == None:
-                
-                    # plot... TODO
-                    # etc.plot_light_curve_SB()
-                    
+                                    
                     test_exposure = 1  # seconds
          
                     counts = etc.calc_counts(obs)
-
                     
                     plot = etc.plot_bodies(obs)
-                    encoded_image = base64.b64encode(plot).decode('utf-8')
-                    # print(encoded_image)
-                    
-                    
-                    # plot = etc.plotTest(obs)
-                    # encoded_image = base64.b64encode(plot).decode('utf-8')
-                    # print(encoded_image)
-                    
-                    
-            
-                    
-                    # plot = etc.plot(obs)
-                    
-           
+                    encoded_image = base64.b64encode(plot).decode('utf-8')                   
                                      
                     signal_values = etc.spreadCounts(obs, counts, 1)
             
-
-            
-                    
-                    # NEW FUNCTION
                     # Adjust counts to what the detector will see (circle)
                     counts = etc.countsInRad(obs, signal_values)
-                    print(counts, "counts per second are recorded for signal to noise calculations.")
-                    print(np.sum(signal_values), "counts per second are recorded from the star in total.")
-                    
-                    
-                    
-                    
-                    
-                    
+                   
                     noise_values = etc.generateNoise(obs ,test_exposure)
                     
                     #TODO rename
@@ -135,41 +81,22 @@ def calculator():
                     
                     final_sensor_array = etc.overfullCheck(signal_values+noise_values+bg_values, obs)
                     
-                    print("The peak number of counts in the simulated image is: ",np.max(final_sensor_array))
-                    print("The minimum number of counts in the simulated image is: ",np.min(final_sensor_array))
-                    
-                    # print("signal values array: ", signal_values)
-                    # print("noise values array: ", noise_values)
-                    # print("bg values array: ", bg_values)
-                    # print("final sensor array: ", final_sensor_array)
-                    
                     peak_cts = np.max(final_sensor_array)
                     min_cts = np.min(final_sensor_array)
                     
                     SNR_ref = etc.get_snr_ref(counts, test_exposure, bg_values, obs)
-                    print("The SNR of the reference image is: ", SNR_ref)   
                     
                     exposure_time = etc.calculateReqTime(obs.snr, SNR_ref, test_exposure, counts, obs, bg_values)
 
                     # exposure_time = "The exposure time is {x:.2f}".format(x=exposure_time)
                     
-                    
                     aperture_plot = etc.aperturePlot(obs, final_sensor_array)
                     aperture_plot_encoded = base64.b64encode(aperture_plot).decode('utf-8')
-                    
-                    
+                     
                     fov = obs.computeFOV()
-                    
-                    
-                    
-                    
-                    # new_url = etc.plotTest(obs)
-                    # print(new_url)
-                    # encoded_image = base64.b64encode(plot).decode('utf-8')
                     
                 
                     # rounding
-                    
                     try:
                         counts = round(counts)
                         peak_cts = round(peak_cts)
@@ -178,8 +105,7 @@ def calculator():
                     except:
                         pass
                     
-                    
-                    
+    
                     # table formatting
                     table_1a = []
                     table_1b = []
@@ -196,14 +122,11 @@ def calculator():
                             table_2b.append(params[key]) 
                         i += 1
                                                                
-                   
-                     #    SB_url="static/plot_light_curve_SB.png"
                     
                     # render output template
                     return render_template('output.html', valid=valid, in_form=in_form, select_form=select_form,
                                             camera_presets=camera_presets, telescope_presets=telescope_presets,
-                                            gao_sqm=gao_sqm,
-                                            plot=encoded_image, aperture=aperture_plot_encoded,
+                                            gao_sqm=gao_sqm, plot=encoded_image, aperture=aperture_plot_encoded,
                                             counts=counts, exposure=exposure_time, peak=peak_cts, minimum=min_cts, fov=fov, 
                                             col1a=table_1a, col1b=table_1b, col2a=table_2a, col2b=table_2b)
                                             #fov=fov, counts=counts, peak=peak, minimum=minimum, exposure=exposure, error=None)
@@ -223,47 +146,6 @@ def calculator():
                            camera_presets=camera_presets, telescope_presets=telescope_presets, 
                            gao_sqm=gao_sqm, 
                            error="Invalid Parameter(s) Below")
-
-
-
-def readPresets(file_name: str) -> list:
-    """    
-    Read data from template csv files
-    
-    Args:
-        file_name (str): name of parameter category (i.e. 'camera', 'telescope', etc.)
-
-    Returns:
-        list: parameters from template
-        None: error reading template
-    """
-    
-    try:
-        csv = open("./static/presets/" + file_name + "_presets.csv", "+r")
-        presets = []
-        
-        # skip the preset template instructions
-        csv.readline()
-        csv.readline()
-        csv.readline()
-        
-        # iterate through lines in file
-        for line in csv:
-            
-            line = line.strip().split(":")
-            name = line[0]
-            values = line[1].split(',')
-            
-            # validate parameter count 
-            if( len(values) == InputForm.fields[file_name] ):
-                presets.append( (name, values) )
-        
-        csv.close()
-        return presets
-    
-    except: return None
-
-
 
 
 
@@ -294,32 +176,7 @@ def process_input(input: dict) -> dict:
 
     return params
     
-        
-    
-    
-    
-    
-    
-
-
-#TODO move to etc script?
-# def pickles(): 
-    
-#     file = open("./static/presets/pickles.csv")
-#     presets = []
-    
-#     for line in file:    
-#         csvs = line.strip().split(",")
-        
-        
-#         presets.append( (csvs[1], [csvs[2]]) )
-        
-#     file.close()
-#     return presets
-        
-    
-
-
+            
 
 if __name__ == '__main__':
 
