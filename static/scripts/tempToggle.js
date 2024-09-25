e21e721263864717b074ee2b20aaac29f40cd9af
pirgalim <109841774+pirgalim@users.noreply.github.com>
2024-09-24 21:56:36 -0500

    fixed temperature required bug

--- static/scripts/tempToggle.js
+++ static/scripts/tempToggle.js
@@ -33,33 +33,60 @@ document.getElementById('point_src').addEventListener('change', function() {
 });
 
 
-window.onload = changeTemp;
+document.addEventListener('DOMContentLoaded', function() {
+    
+    document.getElementById("defaultClose").addEventListener("click", function() {
 
-function changeTemp() {
 
-
-    var selectedOption = document.getElementById('point_src').value;
-
-    if(selectedOption != '') {
-
+        console.log("Hello")
         tempLabel.style.background = "lightgray";
         tempLabel.style.color = "darkgray";
         tempField.style.background = "lightgray";
-        // tempField.style.border = "lightgray";
+        tempField.style.border = "lightgray";
         tempField.style.color = "lightgray";
         tempField.required = false;
         tempField.readOnly = true;
-    }
+    })
 
-    else {
 
-        tempLabel.style.background = "slategray";
-        tempLabel.style.color = "white";
-        tempField.style.background = "white";
-        // tempField.style.border = "white";
-        tempField.style.color = "black";
-        tempField.required = true;
-        tempField.readOnly = false;
-    }
+
+
+
+
+});
+
+// window.onload = changeTemp;
+
+
+
+
+
+
+// function changeTemp() {
+
+
+//     var selectedOption = document.getElementById('point_src').value;
+
+//     if(selectedOption != '') {
+
+//         tempLabel.style.background = "lightgray";
+//         tempLabel.style.color = "darkgray";
+//         tempField.style.background = "lightgray";
+//         // tempField.style.border = "lightgray";
+//         tempField.style.color = "lightgray";
+//         tempField.required = false;
+//         tempField.readOnly = true;
+//     }
+
+//     else {
+
+//         tempLabel.style.background = "slategray";
+//         tempLabel.style.color = "white";
+//         tempField.style.background = "white";
+//         // tempField.style.border = "white";
+//         tempField.style.color = "black";
+//         tempField.required = true;
+//         tempField.readOnly = false;
+//     }
     
-}
+// }
