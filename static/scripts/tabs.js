c840f4214509f321c44fcfcb5df685d143f05a96
pirgalim <109841774+pirgalim@users.noreply.github.com>
2025-05-16 21:30:09 -0500

    working colour tabs

--- static/scripts/tabs.js
+++ static/scripts/tabs.js
@@ -23,15 +23,18 @@ function Tab(button, name) {
 
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
+
     for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
+
     for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+    
     document.getElementById(name).style.display = "flex";
-    button.currentTarget.className += " active";
+    button.classList.add("active");
 
 
 
